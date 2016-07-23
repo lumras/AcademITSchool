90bc8c9d67c5d0c398210e6901a7f06f333708f1
lumras <splinter999@mail.ru>
2016-07-23 08:41:25 +0600

    First commit

---
+++ Shapes/src/ru/academit/busov/shapes/Main.java
@@ -0,0 +1,10 @@
+package ru.academit.busov.shapes;
+
+/**
+ * Created by 1 on 23.07.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
